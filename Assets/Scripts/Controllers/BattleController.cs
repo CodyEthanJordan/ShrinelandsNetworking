f20e3482b2ca4a02bcb50c426388be6b27c35158
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-09-22 22:56:43 -0400

    Camera movement

--- Assets/Scripts/Controllers/BattleController.cs
+++ Assets/Scripts/Controllers/BattleController.cs
@@ -13,6 +13,10 @@ namespace Assets.Scripts.Controllers
     public class BattleController : MonoBehaviour
     {
         public GameObject BlockPrefab;
+        public Camera Cam;
+        public float Speed;
+        public SmoothMouseLook mouseLook;
+
 
         [SerializeField] private Text serverIPText;
         [SerializeField] private GameObject connectionPanel;
@@ -24,6 +28,32 @@ namespace Assets.Scripts.Controllers
         {
             client.OnConnected += Connected;
             client.OnRecieveBattle += InitializeBattle;
+
+            mouseLook.enabled = false;
+        }
+
+        private void Update()
+        {
+            HandleCameraMovement();
+        }
+
+        private void HandleCameraMovement()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                mouseLook.enabled = true;
+                Cursor.visible = false;
+            }
+            else if (Input.GetMouseButtonUp(1))
+            {
+                mouseLook.enabled = false;
+                Cursor.visible = true;
+            }
+
+            Cam.transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Speed * Time.deltaTime, Space.Self);
+            Cam.transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Speed * Time.deltaTime, Space.Self);
+            Cam.transform.Translate(Vector3.up * Input.GetAxis("Jump") * Speed * Time.deltaTime, Space.World);
+
         }
 
         private void CleanUp()
@@ -46,9 +76,9 @@ namespace Assets.Scripts.Controllers
                     for (int k = 0; k < battle.map.Shape[2]; k++)
                     {
                         var newBlock = Instantiate(BlockPrefab, this.transform, true);
-                        newBlock.transform.position = new Vector3(i, j, k);
+                        newBlock.transform.position = new Vector3(i, k, j);
                         var br = newBlock.GetComponent<BlockRenderer>();
-                        br.Become(battle.map.Blocks[i][k][j].Name);
+                        br.Become(battle.map.Blocks[i][j][k].Name);
                         blocks.Add(br);
                     }
                 }
