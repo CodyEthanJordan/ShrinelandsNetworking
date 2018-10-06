6c9b71df97b6598eaf2e40a1a6de4646f872f3b7
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-10-06 00:43:57 -0400

    Unit tests work

---
+++ Tests/BattleTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BattleTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BattleTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("09096508-9509-44bc-92b4-2dc9a910ccc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/BattleTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assets.Scripts.DungeonMaster;
+
+namespace BattleTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BattleExists()
+        {
+            Battle b = Battle.GetDebugBattle();
+            Assert.IsNotNull(b);
+        }
+    }
+}
