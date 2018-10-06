c9148b12ec59bae2b06ec6172e0619cadd7215be
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-10-06 02:02:53 -0400

    Goodnight

--- Tests/BattleTests/OverallBattleTests.cs
+++ Tests/BattleTests/OverallBattleTests.cs
@@ -31,6 +31,7 @@ namespace BattleTests
             Battle b = Battle.GetDebugBattle();
             Unit charlie = b.units.First(u => u.Name == "Charlie");
             Unit jj = b.units.First(u => u.Name == "JJ");
+            Assert.IsTrue(jj.HP.Current == jj.HP.Max);
             b.MakeMove(charlie.ID, Map.Direction.North);
             b.BasicAttack(charlie.ID, Map.Direction.North, 0); //guaranteed hit
             Assert.IsTrue(jj.HP.Current < jj.HP.Max);
