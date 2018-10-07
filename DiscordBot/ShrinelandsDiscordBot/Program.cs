f74d8ddf9f635a5b73fcd8e1fa2f05a02ff2a80e
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-10-07 01:07:22 -0400

    Setting up bot

--- DiscordBot/ShrinelandsDiscordBot/Program.cs
+++ DiscordBot/ShrinelandsDiscordBot/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
 
 namespace ShrinelandsDiscordBot
 {
     class Program
     {
+        static DiscordClient discord;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "<your token here>",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
