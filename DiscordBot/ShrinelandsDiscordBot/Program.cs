c25db4557f80a9f8418fee78d0dca345835349b8
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-10-07 01:14:41 -0400

    Discord bot lives

--- DiscordBot/ShrinelandsDiscordBot/Program.cs
+++ DiscordBot/ShrinelandsDiscordBot/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using Assets.Scripts.DungeonMaster;
 using DSharpPlus;
 
 namespace ShrinelandsDiscordBot
@@ -7,24 +9,38 @@ namespace ShrinelandsDiscordBot
     class Program
     {
         static DiscordClient discord;
+        private static string key;
+        private static Battle battle;
 
         static void Main(string[] args)
         {
+            TextReader tr = new StreamReader(@"Key.txt");
+            key = tr.ReadLine();
+
+            battle = DebugData.GetFunDebugBattle();
+
             MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+
         }
 
         static async Task MainAsync(string[] args)
         {
             discord = new DiscordClient(new DiscordConfiguration
             {
-                Token = "<your token here>",
-                TokenType = TokenType.Bot
+                Token = key,
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
             });
 
             discord.MessageCreated += async e =>
             {
                 if (e.Message.Content.ToLower().StartsWith("ping"))
                     await e.Message.RespondAsync("pong!");
+                else if (e.Message.Content.ToLower() == "map")
+                {
+                    await e.Message.RespondAsync(battle.ShowBattle().Substring(0,100));
+                }
             };
 
             await discord.ConnectAsync();
